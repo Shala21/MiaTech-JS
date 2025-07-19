07977e480ba89c82b09e44756146934065c7860e
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-07-19 11:13:25 +0200

    feat: add ex. and clean useless files

--- lesson.js-2/script.js
+++ lesson.js-2/script.js
@@ -1,4 +1,5 @@
-//Exercise -> 1
+//Exercise 1, 2, 3 -->
+
 class Automobile {
     constructor(marca, modello, anno) {
         this.marca = marca;
@@ -11,17 +12,55 @@ class Automobile {
     }
 }
 
-let auto = new Automobile("Mercedes", "CLA 200", 2024);
-console.log(auto.descrizione());
-
 class Chilometraggio extends Automobile {
-    chilometri = 
-    constructor(marca, modello, anno, chilometri) {
+    constructor(marca, modello, anno, chilometri = 0) {
         super(marca, modello, anno);
-        this.chilometri = chilometri
+        this.chilometri = chilometri;
     }
-    let 
+    aggiungiChilometri(km) {
+        if (km > 0) {
+            this.chilometri += km;
+        } else {
+            console.log("Not valid for KM.");
+        }
+    }
+    mostraChilomeraggio() {
+        return `${this.chilometri} km`;
+    }
+} 
+
+
+let auto = new Chilometraggio("Mercedes", "CLA 200", 2024);
+
+console.log(auto.descrizione());
+console.log(auto.mostraChilomeraggio());
+
+auto.aggiungiChilometri(150);
+console.log(auto.mostraChilomeraggio());
+
+class Elettrica extends Automobile {
+    constructor(marca, modello, anno, chilometri = 0, autonomia = 300) {
+        super(marca, modello, anno);
+        this.chilometri = chilometri;
+        this.autonomia = autonomia;
+    }
+
+    ricarica(km) {
+        if (km > 0) {
+            this.autonomia += km;
+        } else {
+            console.log("Out of charge.");
+        }
+    }
+
+    mostraAutonomia() {
+        return `${this.autonomia} km di autonomia`;
+    }
 }
 
-// Exercise -> 
 
+let carica = new Elettrica("Tesla", "Model Y", 2024, 10000, 400);carica.ricarica(20);
+console.log(carica.descrizione());
+console.log(carica.mostraAutonomia());
+carica.ricarica(10);
+console.log(carica.mostraAutonomia());
