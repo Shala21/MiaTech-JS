12377e15b4d28b23c0a4ee48567c8f671a53120b
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-07-29 18:01:07 +0200

    feat: implementato #incrementaContatore e integrazione in aggiungiChilometri(km)

--- lesson.js-2/script.js
+++ lesson.js-2/script.js
@@ -1,4 +1,5 @@
 class Automobile {
+    // Exercise 1 - 2 & 5 -  ->
     #contatoreChiamate;
 
     constructor(marca, modello, anno, chilometri = 0) {
@@ -7,7 +8,7 @@ class Automobile {
         this.anno = anno;
         this.chilometri = chilometri;
 
-        this.#calcolaEta(); 
+        this.#calcolaEta();
         this.#contatoreChiamate = 0;
     }
 
@@ -16,16 +17,23 @@ class Automobile {
         this.eta = mostraEta - this.anno;
     }
 
+    // Metodo privato per incrementare il contatore
+    #incrementaContatore() {
+        this.#contatoreChiamate++;
+    }
+
     descrizione() {
         return `${this.marca} ${this.modello} (${this.anno}) - Età: ${this.eta} ${this.eta === 1 ? "anno" : "anni"}`;
     }
 
+    // il metodo privato qui dentro
     aggiungiChilometri(km) {
         if (km > 0) {
             this.chilometri += km;
-            this.#contatoreChiamate++;  
+            this.#incrementaContatore();  
+            // console.log(`Aggiunti ${km} km. Chilometri totali: ${this.chilometri}`)
         } else {
-            console.log("Not valid for KM.");
+            console.log("Not valid for KM."); 
         }
     }
 
@@ -40,6 +48,7 @@ class Automobile {
         return "Chilometraggio sotto controllo.";
     }
 
+    // Exercise 6 ->
     static ConfrontaChilometraggio(auto1, auto2) {
         if (auto1.chilometri > auto2.chilometri) {
             return `${auto1.marca} ${auto1.modello} ha più chilometri di ${auto2.marca} ${auto2.modello}`;
@@ -55,18 +64,19 @@ class Automobile {
     }
 }
 
-
+// Test
 let macc = new Automobile("Mercedes", "CLA 200", 2024);
 macc.aggiungiChilometri(143341);
 macc.aggiungiChilometri(22);
 macc.aggiungiChilometri(36473);
 macc.aggiungiChilometri(4547454);
-console.log(macc.getNumeroChiamate());  
+console.log(macc.getNumeroChiamate());  // Output: 4
 
 let auto1 = new Automobile("Mercedes", "CLA 200", 2024, 100000);
 let auto2 = new Automobile("BMW", "Serie 1", 2021, 1000);
 
-console.log(Automobile.ConfrontaChilometraggio(auto1, auto2))
+console.log(Automobile.ConfrontaChilometraggio(auto1, auto2));
+
 // Exercise 3 ->
 class Elettrica extends Automobile {
     constructor(marca, modello, anno, chilometri = 0, autonomia = 300) {
@@ -91,19 +101,18 @@ class Elettrica extends Automobile {
     }
 }
 
-
 let carica = new Elettrica("Mercedes", "CLA 200", 2020, 120000);
 carica.ricarica(20);
 console.log(carica.descrizione());
-console.log(carica.mostraAutonomia()); 
+console.log(carica.mostraAutonomia());
 carica.ricarica(10);
 console.log(carica.mostraAutonomia());
-console.log(carica.mostraAvvisochilometri());  
-
+console.log(carica.mostraAvvisochilometri());
 
 // Exercise 4 ->
 Automobile.prototype.saluta = function() {
     return `Ciao questa e la mia ${this.marca} ${this.modello} del ${this.anno}!`;
-    };
+};
+
 let macchina = new Automobile("Mercedes", "CLA 200", 2024);
 console.log(macchina.saluta());
