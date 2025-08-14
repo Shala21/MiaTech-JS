8bf570b724df36f415962e6365664b40f96f52f1
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-08-14 10:20:22 +0200

    feat: add  saltare elementi durante il destructing

--- lesson.js-3/script.js
+++ lesson.js-3/script.js
@@ -1,5 +1,7 @@
-// Estrarre elementi base e saltare elementi durante il destructing
+// Estrarre elementi base
 const numeri = [1, 2, 3, 4, 5];
 
-const [a, b] = numeri;
+const [a, b, c, d, e] = numeri;
 console.log(a, b);
+// saltare elementi durante il destructing
+console.log(a, c);
