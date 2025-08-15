40d07f0a1c39a2349a32288f7c7b4e17196dd8d2
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-08-15 09:23:56 +0200

    feat: Destructing con valori predefiniti

--- lesson.js-3/script.js
+++ lesson.js-3/script.js
@@ -1,7 +1,20 @@
 // Estrarre elementi base
 const numeri = [1, 2, 3, 4, 5];
 
-const [a, b, c, d, e] = numeri;
+const [a, b, c] = numeri;
 console.log(a, b);
+
 // saltare elementi durante il destructing
 console.log(a, c);
+
+// Destructing con valori predefiniti
+const fruits = ["banana", "mela", "kiwi"];
+const [
+  fruit1 = "default1",
+  fruit2 = "default2",
+  fruit3 = "default3",
+  fruit4 = "default4",
+  fruit5 = "default5"
+] = fruits;
+
+console.log(fruit1, fruit2, fruit3, fruit4, fruit5);
