3d1980959b02a869f0b879433c431db9f18ab699
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-08-20 10:00:59 +0200

    feat: funzioni callback

--- lesson.js-4/script.js
+++ lesson.js-4/script.js
@@ -1,4 +1,14 @@
 // Funzione di base con callback
+function operazione(a, b, callback) {
+    const risultato1 = a * b;
+    console.log("risultatto della moltiplicazione:", risultato1);
+    callback(risultato1);
+}
+operazione(2, 3, function(valore) {
+    console.log("il callback ha ricevuto il valore:" , valore);
+})
+
+// Funzione di callback e passaggio di parametri 
 function somma(a, b, callback) {
     let risultato = a + b;
     callback(risultato);
@@ -7,4 +17,3 @@ function somma(a, b, callback) {
 somma(5, 3, function(risultato) {
     console.log("il risultato della somma è:", risultato);
 })
-
